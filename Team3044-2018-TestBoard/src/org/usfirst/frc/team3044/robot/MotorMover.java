199046ebc23b7cc9673ca0d701867cf399b029b6
Colin <colinator.co@gmail.com>
2018-01-15 10:44:15 -0500

    Working code that is moving 2 motors.
    The RobotDrive class should be used instead of our own code.
    Reading from the smartdashboard has been changed (by WPIlib?) and is not
    working.

--- Team3044-2018-TestBoard/src/org/usfirst/frc/team3044/robot/MotorMover.java
+++ Team3044-2018-TestBoard/src/org/usfirst/frc/team3044/robot/MotorMover.java
@@ -2,13 +2,21 @@ package org.usfirst.frc.team3044.robot;
 
 import org.usfirst.frc.team3044.Reference.*;
 
-import com.ctre.CANTalon;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+
+
+import edu.wpi.first.wpilibj.Talon;
 
 public class MotorMover {
 	FirstController controller = FirstController.getInstance();
 	private Effectors comp = Effectors.getInstance();
-	public CANTalon leftDrive;
-	public CANTalon rightDrive;
+	public WPI_TalonSRX leftDrive;
+	public WPI_TalonSRX rightDrive;
+	double slider;
+	//SmartDashboard dash=new SmartDashboard();
 
 	public void motorMoverInit() {
 		leftDrive = comp.leftDrive;
@@ -17,22 +25,28 @@ public class MotorMover {
 		leftDrive.set(0);
 		rightDrive.set(0);
 
-		rightDrive.enableBrakeMode(true);
-		leftDrive.enableBrakeMode(true);
+		//rightDrive.enableBrakeMode(true);
+		//leftDrive.enableBrakeMode(true);
 
 	}
 
 	public void motorMoverPeriodic() {
-		double x1 = -deadband(controller.getLeftX());
-		double x2 = deadband(controller.getRightX());
+		double y1 = -deadband(controller.getLeftY());
+		double y2 = deadband(controller.getRightY());
 
-		tankDrive(x1, x2);
+		tankDrive(y1, y2);
+		
+		//slider=dash.getNumber("DB/Slider 0", 0);
+		double testSpeed = SmartDashboard.getNumber("DB/Slider 1", 0.9);  //TODO: Not working.
+		System.out.println(testSpeed);
+		
 
+		
 	}
 
-	public void tankDrive(double x1, double x2) {
-		leftDrive.set(x1);
-		rightDrive.set(x2);
+	public void tankDrive(double y1, double y2) {
+		leftDrive.set(y1);
+		rightDrive.set(y2);
 	}
 
 	public double deadband(double value) {
